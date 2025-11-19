1e8f0c6bbee89594730a87f4261145c18728e27f
PavKolacek <pavekolacek@gmail.com>
2025-11-19 17:51:01 +0000

    pridani dalsich her

--- fungovani_automatu/backend_automatu.cpp
+++ fungovani_automatu/backend_automatu.cpp
@@ -3,26 +3,44 @@
 #include "../jednotlive_hry/snake.h"
 #include "../jednotlive_hry/sortinghat.h"
 //#include "tic-tac-toe.cpp"
+#include "../jednotlive_hry/tic_tac_toe.cpp"
+#include "animace.cpp"
 
 int vyber_hry1(){
+int vyber_hry1(){
     int vyberHry;
+    std::cin >> vyberHry;
     std::cin >> vyberHry;
 
     switch (vyberHry)
     {
     case 1:
         hadej_cislo();
+         hadej_cislo();
         break;
     case 2:
         magic8();
+         magic8();
         break;
     case 3:
         sorting_hat();
+         sorting_hat();
         break;
     case 4:
         snake();
+        break;
+    case 5:
+         tic_tac_toe();
     default:
         break;
     }
+    return 0;
+    return 0;
+};
+
+
+
+int backend_automatu(){
+    vyber_hry1();
     return 0;
 }
